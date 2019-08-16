aa0a69dbd149c30dad3753c4462e1192aae3811f
YangHun <pitahano@gmail.com>
2019-08-16 21:11:22 +0900

    implement simple Input Module
    - handles left and right mouse click

---
+++ Assets/Scripts/Input/InputModule.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputModule : MonoBehaviour
+{
+    //use these to assign click event functions 
+    public static System.Action onLeftMouseClicked = null;
+    public static System.Action onRightMouseClicked = null;
+
+    private Coroutine coroutine = null;
+    void Awake() {
+        DontDestroyOnLoad (this.gameObject);
+        if (EventSystem.current == null) this.gameObject.AddComponent<EventSystem>();
+        this.coroutine = StartCoroutine (OnUpdate());
+    }
+
+    private IEnumerator OnUpdate() {
+        while (true) {
+            if (Input.GetMouseButtonDown(0)) onLeftMouseClicked?.Invoke();
+            else if (Input.GetMouseButtonDown(1)) onRightMouseClicked?.Invoke();
+            
+            yield return null;
+        }
+    }
+
+    void OnApplicationPause () {
+        if (this.coroutine != null) {
+            StopCoroutine (this.coroutine);
+            this.coroutine = null;
+        }
+    }
+
+    void OnApplicationFocus() {
+        if (this.coroutine == null) this.coroutine = StartCoroutine (OnUpdate());
+    }
+}
