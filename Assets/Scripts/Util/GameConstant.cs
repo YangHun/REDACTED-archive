8dc05f8653e8a40068d2ecac39c953520ed9a973
YangHun <pitahano@gmail.com>
2019-08-18 04:37:22 +0900

    change good and exact score

--- Assets/Scripts/Util/GameConstant.cs
+++ Assets/Scripts/Util/GameConstant.cs
@@ -5,8 +5,8 @@ public static class GameConstant
     public static readonly float JUDGE_OFFSET_ENTRY = 60f;
     public static readonly float JUDGE_OFFSET_NORMAL = 45f;
     public static readonly float JUDGE_OFFSET_EXACT = 15f;
-    public static readonly int JUDGE_SCORE_0 = 100;
-    public static readonly int JUDGE_SCORE_1 = 50;
+    public static readonly int JUDGE_SCORE_0 = 50;
+    public static readonly int JUDGE_SCORE_1 = 100;
     public static readonly float JUDGE_MISS_LIFE_PENALTY = 0.1f;
     public static readonly float JUDGE_SUCCESS_LIFE_PRICE = 0.02f;
 
