81e70427ff3852c5b3f5833df08989300a78ac93
Phillip Chang <lasagnaphil@snu.ac.kr>
2019-08-18 02:15:34 +0900

    Change title/lobby scene structure

---
+++ Assets/Scripts/UI/TitleUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUI : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button exitButton;
+
+    void Awake()
+    {
+        startButton.onClick.AddListener(() =>
+        {
+            
+        });
+        creditsButton.onClick.AddListener(() =>
+        {
+            
+        });
+        exitButton.onClick.AddListener(() =>
+        {
+            
+        });
+    }
+}
