2afc0695ec47ea97eed9da7f71b88f1d590f7b6f
yukinyaa <yukinyaa712@gmail.com>
2019-08-18 10:58:38 +0900

    Uwin display and fixed channel issue

---
+++ Assets/ScoreDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<UnityEngine.UI.Text>().text = TaikoModule.LastScore + "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
